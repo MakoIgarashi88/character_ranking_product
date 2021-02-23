3ddfae6f8400ccebff055fe0dcd1cfc4c19bc99e
MakoIgarashi88 <igarashi.shu23@gmail.com>
2021-02-23 23:05:19 +0900

    ランキング作成ページの修正 #19

---
+++ resources/js/components/commons/SearchCharacterCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-card class="my-3" height="100%">
+        <v-row justify="center">
+            <v-col cols="auto" class="text-center">
+                <IconSm :src="character.image_name" class="my-5"/>
+            </v-col>
+        </v-row>
+
+
+        <v-divider class="pb-3"/>
+
+        <v-row justify="center">
+            <v-col cols="10" class="text-center pb-0">
+                <router-link :to="{ path: '/character/' + character.id }">
+                    {{ character.name }}
+                </router-link>
+            </v-col>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="10" class="text-center">
+                {{ character.anime_title }}
+            </v-col>
+        </v-row>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        character: Object,
+    },
+}
+</script>
